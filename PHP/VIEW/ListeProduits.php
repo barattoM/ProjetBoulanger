b39b7b7b4a888773cd1663e7e6c3d164f7260d97
barattoM <marvin.baratto@gmail.com>
2020-12-02 15:22:33 +0100

    listeProduits

--- PHP/VIEW/ListeProduits.php
+++ PHP/VIEW/ListeProduits.php
@@ -1,17 +1,25 @@
 <?php
-
-echo '<div class = "colonne">
-<h2>Liste des Films</h2>
-<div class = "liste colonne">';
-echo '<a class="ajout btn" href ="index.php?codePage=formFilm&mode=ajout">Ajouter</a>';
-$films = ProduitsManager::getList();
-foreach ($films as $unFilm)
-{
-    echo '<div class="text">'.$unFilm->getNomFilm().'</div>';
-    echo '<div><div><a class="detail btn" href = "index.php?codePage=formFilm&mode=edit&id='.$unFilm->getIdFilm().'">Detail</a></div>';
-    echo '<div><a class="modif btn" href = "index.php?codePage=formFilm&mode=modif&id='.$unFilm->getIdFilm().'">Modifier</a></div>';
-    echo '<div><a class="suppr btn" href = "index.php?codePage=formFilm&mode=delete&id='.$unFilm->getIdFilm().'">Supprimer</a></div></div>';
-
-}
-echo '</div>';
-
+$produits = ProduitsManager::getList();
+echo '<div class="contenu colonne">
+      <div class="contenuLigne">
+        <div class="espace"></div>
+         <a href="index.php?page=FormulaireProduits&mode=ajout"><div class="ajouter centrer">Ajouter</div> </a>
+        <div class="espace"></div>
+      </div>
+      <div class="colonne">';
+      foreach($produits as $unProduit){
+        echo '<div class="espace"></div>
+            <div class="contenuLigne">
+            <div class="libelle centrer">'.$unProduit->getLibelleProduit().'</div>
+            <div class="espace"></div>
+            <a href="index.php?page=FormulaireProduits&mode=detail&id='.$unProduit->getIdProduit().'"><div class="detail centrer">Detail</div></a>
+            <div class="espace"></div>
+            <a href="index.php?page=FormulaireProduits&mode=modif&id='.$unProduit->getIdProduit().'"><div class="modifier centrer">Modifier</div></a>
+            <div class="espace"></div>
+            <a href="index.php?page=FormulaireProduits&mode=delete&id='.$unProduit->getIdProduit().'"><div class="supprimer centrer">Supprimer</div></a>
+            </div>
+            <div class="espace"></div>';      
+      }
+        
+      echo '</div>
+</div>';
