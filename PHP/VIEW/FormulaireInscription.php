02a5f7dcc54f49ce8cad81b58bc861b0daf5a3ca
MaximeCgn <maxime.cugny@laposte.net>
2020-12-02 15:13:07 +0100

    Maxime 02 12 2020

---
+++ PHP/VIEW/FormulaireInscription.php
@@ -0,0 +1,14 @@
+<?php
+
+echo '<form method="POST action="index.php?page=ActionsUsers&typeAction=inscription">
+<div class="contenu">
+    <div class="espace"></div>
+    <div class="contenuLigne colonne">
+        <div class="ligne colonne centrer">
+            <div class="label">Nom de l\'utilisateur</div>
+            <input type="texte" name="nomUser" placeholder="Nom" class="libelle" required/>
+    </div>
+    
+    <div class="espace"></div>
+</div>
+';
