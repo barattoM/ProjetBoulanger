ba8a568b2604c5e447b95b0dd1a2b7e2829fb808
AamandaLF <laforce.amanda@gmail.com>
2020-12-02 15:37:50 +0100

    ingredients

--- PHP/VIEW/ListeIngredients.php
+++ PHP/VIEW/ListeIngredients.php
@@ -1,34 +1,35 @@
 <?php
 $ingredients = IngredientsManager::getList();
 
-echo '<div class="contenu colonne">
+echo '
+<div class="contenu colonne">
 <div class="contenuLigne">
-  <div class="espace"></div>
-  <div class="ajouter centrer"><a href="index.php?page=FormulaireIngredients&typeFormulaire=ajouter">Ajouter</div>
-  <div class="espace"></div>
+<div class="espace"></div>
+<a href="index.php?page=FormulaireIngredients&mode=ajout"><div class="ajouter centrer">Ajouter</div> </a>
+<div class="espace"></div>
 </div>
-<div>
-  <div class="espace"></div>';
+<div class="colonne">
+';
 
 foreach ($ingredients as $unIngredient)
 {
 
- echo ' <div class="contenuLigne">';
- echo '<div class="libelle centrer">'.$unIngredient->getLibelleIngredient().'</div>';
- echo '<div class="espace"></div>';
- echo '<div class="detail centrer"><a href="index.php?page=FormulaireIngredients&typeFormulaire=details&id='.$unIngredient->getIdIngredient().'">Détails</a></div>';
- echo ' <div class="espace"></div>';
- echo ' <div class="modifier centrer"><a href="index.php?page=FormulaireIngredients&typeFormulaire=modifier&id='.$unIngredient->getIdIngredient().'">Modifier</div>';
- echo ' <div class="espace"></div>';
- echo ' <div class="supprimer centrer"><a href="index.php?page=ActionsIngredients&typeTraitement=supprimer&id='.$unIngredient->getIdIngredient().'">Supprimer</div>';
- echo '</div>';
-
-echo '<div class="espace"></div></div></div>';
-
+echo '
+<div class="espace"></div>
+<div class="contenuLigne">
+<div class="libelle centrer">'.$unIngredient->getLibelleIngredient().'</div>
+<div class="espace"></div>
+<a href="index.php?page=FormulaireIngredients&mode=detail&id='.$unIngredient->getIdIngredient().'"><div class="detail centrer">Detail</div></a>
+<div class="espace"></div>
+<a href="index.php?page=FormulaireIngredients&mode=modif&id='.$unIngredient->getIdIngredient().'"><div class="modifier centrer">Modifier</div></a>
+<div class="espace"></div>
+<a href="index.php?page=FormulaireIngredients&mode=delete&id='.$unIngredient->getIdIngredient().'"><div class="supprimer centrer">Supprimer</div></a>
+</div>
+<div class="espace"></div>';      
 }
-echo '<div class="contenuLigne">';
-echo '<div class="espace"></div>';
-echo '<a href="index.php"><div class="retour centrer">Retour</div></a>';
-echo '<div class="espace"></div>';
-echo '</div>';
+
+echo '
+</div>
+</div>
+';
 
