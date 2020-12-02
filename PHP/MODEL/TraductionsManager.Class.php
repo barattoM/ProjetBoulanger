ccac31ae4b3e8dd54f6bd5ea5c4d4a3c80068245
barattoM <marvin.baratto@gmail.com>
2020-12-02 14:42:12 +0100

    Traductions

--- PHP/MODEL/TraductionsManager.Class.php
+++ PHP/MODEL/TraductionsManager.Class.php
@@ -56,4 +56,23 @@ class TraductionsManager
 		}
 		return $liste;
 	}
+
+	public static function findByCodes($codeLangue,$codeTexte)
+	{
+		$db=DbConnect::getDb();
+		$q=$db->prepare("SELECT texte FROM Traductions WHERE codeTexte =:codeTexte and codeLangue = :codeLangue");
+		$q->bindValue(":codeTexte", $codeTexte,PDO::PARAM_STR);
+		$q->bindValue(":codeLangue", $codeLangue,PDO::PARAM_STR);
+		$q->execute();
+		$results = $q->fetch(PDO::FETCH_ASSOC);
+		if($results != false)
+		{
+			
+			return $results['texte'];  // on ne retourne que le texte, pas un objet
+		}
+		else
+		{
+			return false;
+		}
+	}
 }
